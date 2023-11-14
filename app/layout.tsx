c14d16e1e01b887b8921b1ed5bc8a29e592a8ca9
vasanth2320 <vasanthkumars2025@gmail.com>
2023-11-14 17:39:52 +0530

    create auth and dark theme feature

---
+++ app/layout.tsx
@@ -0,0 +1,40 @@
+import type { Metadata } from "next";
+import { Open_Sans } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
+import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const font = Open_Sans({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Discord Clone",
+  description: "Team Chat Application",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ClerkProvider>
+        <body className={cn(font.className, "bg-white dark:bg-[#313338]")}>
+          {/* TODO: I have remove comment parameter in ThemeProvider opening tag */}
+          <ThemeProvider
+            attribute="class"
+            // defaultTheme="dark"
+            forcedTheme="dark"
+            enableSystem={false}
+            storageKey="discord-theme"
+            // disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
+      </ClerkProvider>
+    </html>
+  );
+}
